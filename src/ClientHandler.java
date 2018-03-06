fad961c7faff4da388c97feaacd34be2272163f2
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2018-03-06 12:58:28 +0100

    Minor improvements

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,18 +1,16 @@
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class ClientHandler implements Handler {
     private final Socket clientSocket;
+    private static ScriptEngineManager mgr = new ScriptEngineManager();
+    private static ScriptEngine engine = mgr.getEngineByName("JavaScript");
 
     ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -30,19 +28,14 @@ public final class ClientHandler implements Handler {
             writer.println("Give me a simple arithmetic expression to calculate.");
 
             /* Receives data from client */
-            ScriptEngineManager mgr = new ScriptEngineManager();
-            ScriptEngine engine = mgr.getEngineByName("JavaScript");
             String line = reader.readLine();  // Receives a line of text.
             while (line != null) {
-                String result = "";
+                // Respond to client with result.
                 try {
-                    result = engine.eval(line).toString();
-                } catch (Exception e) {
+                    writer.println("Result of '" + line + "' is " + engine.eval(line).toString());
+                } catch (ScriptException e) {
                     e.printStackTrace();
                 }
-
-            // Respond to client with result.
-                writer.println("Result of '" + line + "' is " + result);
 
                 // Read another line.
                 line = reader.readLine();
