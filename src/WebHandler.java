367c9bbb5e33ce7c3f3c6a4fb508f01e2580fd1c
Daniel Klock <daniel.a.klock@gmail.com>
2018-03-06 13:21:40 +0100

    Add header output for WebHandler

--- src/WebHandler.java
+++ src/WebHandler.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class WebHandler implements Handler {
     private final Socket webSocket;
@@ -13,8 +14,21 @@ public class WebHandler implements Handler {
         try (InputStream reader = webSocket.getInputStream();
              OutputStream writer = webSocket.getOutputStream()) {
 
-        writer.write(("HTTP/1.1 200 OK\n\n" +
-                "<h1>Welcome to the Server</h1>").getBytes());
+            Scanner headerScanner = new Scanner(reader).useDelimiter("\\n");
+            String header = headerScanner.hasNext() ? headerScanner.next() : "";
+
+        writer.write(("HTTP/1.1 200 OK Content-Type: text/html\n\n" +
+                "<html>" +
+                    "<head>" +
+                        "<title>Socket Server</title>" +
+                    "</head>" +
+                    "<body>" +
+                        "<h1>Welcome to the Server</h1>" +
+                        "<ul>" +
+                            "<li>" + header + "</li>" +
+                        "</ul>" +
+                    "</body>" +
+                "</html>").getBytes());
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
