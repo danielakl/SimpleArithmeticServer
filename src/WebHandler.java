d43266164856c69a579a8ec429328943584d456a
Daniel Klock <daniel.a.klock@gmail.com>
2018-03-06 13:42:38 +0100

    Add header output to website from client.

--- src/WebHandler.java
+++ src/WebHandler.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WebHandler implements Handler {
@@ -14,21 +16,33 @@ public class WebHandler implements Handler {
         try (InputStream reader = webSocket.getInputStream();
              OutputStream writer = webSocket.getOutputStream()) {
 
-            Scanner headerScanner = new Scanner(reader).useDelimiter("\\n");
-            String header = headerScanner.hasNext() ? headerScanner.next() : "";
+            List<String> headers = new ArrayList<>();
+            Scanner headerScanner = new Scanner(reader);
+            String header = headerScanner.nextLine();
+            while (!header.equals("")) {
+                headers.add(header);
+                header = headerScanner.nextLine();
+            }
 
-        writer.write(("HTTP/1.1 200 OK Content-Type: text/html\n\n" +
-                "<html>" +
+            StringBuilder sb = new StringBuilder();
+            for (String s : headers) {
+                sb.append("<li>");
+                sb.append(s);
+                sb.append("</li>");
+            }
+
+            writer.write(("HTTP/1.1 200 OK Content-Type: text/html\n\n" +
+                    "<html>" +
                     "<head>" +
-                        "<title>Socket Server</title>" +
+                    "<title>Socket Server</title>" +
                     "</head>" +
                     "<body>" +
-                        "<h1>Welcome to the Server</h1>" +
-                        "<ul>" +
-                            "<li>" + header + "</li>" +
-                        "</ul>" +
+                    "<h1>Welcome to the Server</h1>" +
+                    "<ul>" +
+                        sb.toString() +
+                    "</ul>" +
                     "</body>" +
-                "</html>").getBytes());
+                    "</html>").getBytes());
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
